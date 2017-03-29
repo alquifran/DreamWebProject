065a7a3d6b519ccd9729cbb4fa5bcc428f021d1d
fran <fran.blasco94@gmail.com>
2017-03-29 17:40:57 +0200

    Revisión

--- app/Controller/ClientController.php
+++ app/Controller/ClientController.php
@@ -28,52 +28,6 @@ class ClientController
 		$view->render('profile.php', ['pageTitle'=>"Perfil"]);
 	}
 
-	// public function index()
-	// {
-
-	// 	$view = new View('templates/piso');
-	// 	$pisos = Piso::all();
-	// 	$view->render('index.php', ['pisos' => $pisos, 'pageTitle'=>"Listado de pisos"]);
-	// }
-
-	// public function show($id)
-	// {
-	// 	$view = new View('templates/piso');
-	// 	$piso = Piso::find($id);
-	// 	$view->render('show.php', ['piso' => $piso]);
-	// }
-
-	// public function delete($id)
-	// {
-	// 	$view = new View('templates/piso');
-	// 	Piso::delete($id);
-	// 	header('Location:../');
-	// }
-
-	// public function add(){
-	// 	if(empty($_POST)){
-	// 		$view = new View('templates/piso');
-	// 		$view->render('add.php', []);
-	// 	}
-	// 	else{
-			
-	// 		Piso::add(null, $_POST['direccion'], $_POST['precio'], $_POST['descripcion']);
-	// 		$_POST = "";
-	// 		header('Location:../');
-	// 	}
-	// }
-
-	// public function update($id){
-	// 	if(empty($_POST)){
-	// 		$view = new View('templates/piso');
-	// 		$piso = Piso::find($id);
-	// 		$view->render('update.php', ['piso' => $piso]);
-	// 	}
-	// 	else{
-	// 		Piso::update($id, $_POST['direccion'], $_POST['precio'], $_POST['descripcion']);
-	// 		$_POST = "";
-	// 		header('Location:../');
-	// 	}
-	// }
+	
 	
 }
--- app/templates/clients/index.php
+++
@@ -1,8 +0,0 @@
-<h1>Listado de clientes</h1>
-<ul>
-	<?php foreach ($pisos as $piso): ?>
-		<li><a href='show/<?= $piso->getId(); ?>'><?= $piso->getDireccion() ?></a></li>
-	<?php endforeach ?>
-</ul>
-
-<a href='add'><button>Añadir piso</button></a>
